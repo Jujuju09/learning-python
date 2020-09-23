397be63021960f58706b87e468507e77b4c200c0
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-09-23 15:45:06 +0800

    更新sublime破解码和day2部分内容

---
+++ DAY1-15/day2.py
@@ -0,0 +1,39 @@
+# 数据类型
+# 1.整型
+# 2.浮点型
+# 3.字符串型
+# 4.布尔值
+# 5.复数型：3+5j
+
+# 基本算数
+# a=987
+# b=21
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# 判断字符串类型
+# a=99
+# b=12.123
+# c=1+5j
+# d='hello'
+# e=False
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print('%d + %d = %d' % (a, b, a + b))
+# print('%d - %d = %d' % (a, b, a - b))
+# print('%d * %d = %d' % (a, b, a * b))
+# print('%d / %d = %f' % (a, b, a / b))
+# print('%d // %d = %d' % (a, b, a // b))
+# print('%d %% %d = %d' % (a, b, a % b))
+# print('%d ** %d = %d' % (a, b, a ** b))
+
+# %d是整数占位符，%f是浮点数占位符
+# //是整除，%%是取余，**幂运算
