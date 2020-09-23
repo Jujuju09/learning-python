aef5a5c58352e3508fc96fa0c78ab59aae7b4d21
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-09-23 18:15:14 +0800

    更新部分day3学习内容

---
+++ DAY1-15/day3.py
@@ -0,0 +1,44 @@
+# # 身份验证
+
+# username=input("username:")
+# password=input("password:")
+
+# if username== "admin" and password=="12345":
+#     print("pass")
+# else:
+#     print("failed")
+
+# 分段函数求值
+
+# x=float(input("请输入x的值："))
+
+# # if x>1:
+# #     print(3*x-5)
+# # elif -1<=x<=1:
+# #     print(x+2)
+# # else:
+# #     print(5*x+3)
+# if x>1:
+#     y=3*x-5
+# elif -1<=x<=1:
+#     y=x=2
+# else:
+#     y=5*x+3
+# print('f(%.2f) = %.2f' % (x, y))
+
+# # print(y)
+
+# 英制单位与公制单位厘米互换
+x=float(input("请输入长度："))
+a=input("请输入单位（英尺/厘米）：")
+
+if a=="英尺" or a=="in":
+    print(x*2.54)
+    
+elif a=="厘米" or a=="cm":
+    print(x/2.54)
+   
+else:
+    print("请输入正确的单位")
+
+    
