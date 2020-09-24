3e8cf2f74930ba77b7d99a011c4d2f568977a3de
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-09-24 18:41:19 +0800

    更新day4练习

---
+++ DAY1-15/day4_practice.py
@@ -0,0 +1,71 @@
+# 用for循环实现1~100求和
+# sum=0
+# x=0
+# for x in range(0,101):
+#     sum +=x
+# print(sum)
+
+# 用for循环实现1~100之间的偶数求和
+
+# sum=0
+# for x in range(2,101,2):
+#     sum += x
+# print(sum)
+
+# sum=0
+# for x in range(1,101):
+#     if x%2==0:
+#         sum +=x
+# print(sum)
+
+# 输入非负整数n计算n!
+# mul=1
+# n=int(input("num="))
+# for x in range(1,n+1):
+#     mul *=x
+# print(mul)
+
+# 输入一个正整数判断它是不是素数
+y=True
+n=int(input("n="))
+for i in range(2,n):
+    if n%i==0:
+        y= False
+        break
+else:
+    y=True
+print(y)
+
+
+# for a in range(1,10):
+#     print (a)
+
+ 
+
+
+# 输入两个正整数计算最大公约数和最小公倍数
+
+
+
+# 打印各种三角形图案
+# *
+# **
+# ***
+# ****
+# *****
+#     *
+#    **
+#   ***
+#  ****
+# *****
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+
+# 用while循环实现1~100求和
+
+
+# 用while循环实现1~100之间的偶数求和
