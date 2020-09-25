d18093a233d81a8392a021c857802b8b57ae74f3
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-09-25 20:58:38 +0800

    update

--- DAY1-15/day4_practice.py
+++ DAY1-15/day4_practice.py
@@ -26,15 +26,29 @@
 # print(mul)
 
 # 输入一个正整数判断它是不是素数
-y=True
-n=int(input("n="))
-for i in range(2,n):
-    if n%i==0:
-        y= False
-        break
-else:
-    y=True
-print(y)
+# y=True
+# n=int(input("n="))
+# for i in range(2,n):
+#     if n%i==0:
+#         y= False
+#         break
+# else:
+#     y=True
+# print(y)
+
+# from math import sqrt
+# num=int(input("num="))
+# mid=int(sqrt(num))
+# is_prime=True
+# #mid用来减少循环次数
+# for x in(2,mid+1):
+#     if num%x==0:
+#         is_prime=False
+#         break
+# if is_prime==True and num !=1:
+#      print("是素数")
+# else:
+#     print("不是素数")
 
 
 # for a in range(1,10):
@@ -45,7 +59,27 @@ print(y)
 
 # 输入两个正整数计算最大公约数和最小公倍数
 
+# num1=int(input("num1="))
 
+# num2=int(input("num2="))
+# if num1>num2:
+#     (num1,num2)=(num2,num1)
+
+# for a in range(num1,0,-1):
+#     if num1%a==0 and num2%a==0:
+#         print(a)
+#         print(num1*num2//a)
+#         break
+
+# x = int(input('x = '))
+# y = int(input('y = '))
+# if x > y:
+#     (x, y) = (y, x)
+# for factor in range(x, 0, -1):
+#     if x % factor == 0 and y % factor == 0:
+#         print('%d和%d的最大公约数是%d' % (x, y, factor))
+#         print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+#         break
 
 # 打印各种三角形图案
 # *
@@ -64,8 +98,27 @@ print(y)
 #  *******
 # *********
 
+row = int(input('请输入行数: '))
+for i in range(row):
+    for _ in range(i + 1):
+        print('*', end='')
+    print()
 
 # 用while循环实现1~100求和
-
+# sum=0
+# num=0
+# while num<=100:
+#     sum +=num
+#     num +=1
+# print(sum)
+    
 
 # 用while循环实现1~100之间的偶数求和
+
+# sum=0
+# num=1
+# while num<=100:
+#     sum +=num
+#     num +=2
+# print(sum)
+
