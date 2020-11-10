c527abcba4a145a714476f03251c3b97e677d2a3
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-11-10 18:13:15 +0800

    update

--- DAY1-15/day7_practice.py
+++ DAY1-15/day7_practice.py
@@ -118,21 +118,90 @@
 
 
 # 练习4：设计一个函数返回传入的列表中最大和第二大的元素的值。
-def max2(x):
-    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
-    #保证m1>m2，即m1是最大数,m2是第二大
-    for index in range(2, len(x)):
-        if x[index] > m1:
-            m2 = m1
-            m1 = x[index]
-        elif x[index] > m2:
-            m2 = x[index]
-    return m1, m2
-x=[2,4,3,1]
-print(max2(x))
+# def max2(x):
+#     m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+#     #保证m1>m2，即m1是最大数,m2是第二大
+#     for index in range(2, len(x)):
+#         #找下标在2~len(x)之间的元素
+#         if x[index] > m1:
+#             m2 = m1
+#             m1 = x[index]
+#         elif x[index] > m2:
+#             m2 = x[index]
+#     return m1, m2
+# x=[2,4,3,1]
+# print(max2(x))
+
+#练习
+# def max1_2(x):
+#     m1,m2=(x[0],x[1]) if x[0]>x[1] else (x[1],x[0])
+#     for index in range (2,len(x)):
+#         if x[index]>m1:
+#             m2=m1
+#             m1=x[index]
+#         elif x[index]>m2:
+#             m2=x[index]
+#     return m1,m2
+# x=[3,4,5,7,22,3]
+# print(max1_2(x))
+
+##练习*2，原来这就是冒泡排序啊
+
+# def maxnum2(x):
+#     m1,m2=(x[0],x[1]) if x[0]>x[1] else(x[1],x[0])
+#     for index in range (2,len(x)):
+#         if x[index]>m1:
+#             m1=m2
+#             m1=x[index]
+#         elif x[index]>m2:
+#             m2=x[index]
+#     return m1,m2
+# x=[99,2,3,8,10]
+# print(maxnum2(x))
+
+
+
+
 
 # 练习5：计算指定的年月日是这一年的第几天
+def is_leap_year(year):
+    """
+    判断指定的年份是不是闰年
 
+    :param year: 年份
+    :return: 闰年返回True平年返回False
+    """
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def which_day(year, month, date):
+    """
+    计算传入的日期是这一年的第几天
+
+    :param year: 年
+    :param month: 月
+    :param date: 日
+    :return: 第几天
+    """
+    days_of_month = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ][is_leap_year(year)]
+    total = 0
+    for index in range(month - 1):
+        total += days_of_month[index]
+    return total + date
+
+
+def main():
+    print(which_day(1980, 11, 28))
+    print(which_day(1981, 12, 31))
+    print(which_day(2018, 1, 1))
+    print(which_day(2016, 3, 1))
+
+
+if __name__ == '__main__':
+    main()
 
 # 练习6：打印杨辉三角。
 
