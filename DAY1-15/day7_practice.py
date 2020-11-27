d5614ef89fd1a5bb4f0ff1236ac575d91689fc14
NTTDATABJ\Thea.Fan <Thea.Fan@china.nttdata.com>
2020-11-27 17:02:54 +0800

    update

--- DAY1-15/day7_practice.py
+++ DAY1-15/day7_practice.py
@@ -164,46 +164,85 @@
 
 
 # 练习5：计算指定的年月日是这一年的第几天
-def is_leap_year(year):
-    """
-    判断指定的年份是不是闰年
 
-    :param year: 年份
-    :return: 闰年返回True平年返回False
-    """
-    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+# def is_leap_year(year):
+#     """
+#     判断指定的年份是不是闰年
 
-
-def which_day(year, month, date):
-    """
-    计算传入的日期是这一年的第几天
-
-    :param year: 年
-    :param month: 月
-    :param date: 日
-    :return: 第几天
-    """
-    days_of_month = [
-        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    ][is_leap_year(year)]
-    total = 0
-    for index in range(month - 1):
-        total += days_of_month[index]
-    return total + date
+#     :param year: 年份
+#     :return: 闰年返回True平年返回False
+#     """
+#     return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
 
-def main():
-    print(which_day(1980, 11, 28))
-    print(which_day(1981, 12, 31))
-    print(which_day(2018, 1, 1))
-    print(which_day(2016, 3, 1))
+# def which_day(year, month, date):
+#     """
+#     计算传入的日期是这一年的第几天
+
+#     :param year: 年
+#     :param month: 月
+#     :param date: 日
+#     :return: 第几天
+#     """
+#     days_of_month = [
+#         [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+#         [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     ][is_leap_year(year)]
+#     total = 0
+#     for index in range(month - 1):
+#         total += days_of_month[index]
+#     return total + date
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     print(which_day(1980, 11, 28))
+#     print(which_day(1981, 12, 31))
+#     print(which_day(2018, 1, 1))
+#     print(which_day(2016, 3, 1))
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+# #判断是否是闰年
+# def is_leap_year(year):
+#     return year%4==0 and year%100 !=0 or year%400==0 
+
+
+# #判断是第几天
+# def which_day(year,month,date):
+#     days_of_month=[
+#         [31,28,31,30,31,30,31,31,30,31,30,31],
+#         [31,29,31,30,31,30,31,31,30,31,30,31]
+#     ][is_leap_year(year)]
+#     total=0
+#     for index in range(month-1):
+#         total +=days_of_month[index]
+#     return total+date
+# print(which_day(2020,3,1))
+
+
 
 # 练习6：打印杨辉三角。
+# def main():
+#     num = int(input('Number of rows: '))
+#     yh = [[]] * num
+#     for row in range(len(yh)):
+#         yh[row] = [None] * (row + 1)
+#         for col in range(len(yh[row])):
+#             if col == 0 or col == row:
+#                 yh[row][col] = 1
+#             else:
+#                 yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+#             print(yh[row][col], end='\t')
+#         print()
+
+
+# if __name__ == '__main__':
+#     main()
+
 
 
 
